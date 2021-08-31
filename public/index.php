6c53066da6a4f5843f2e5e0794af97350db27d51
gyan <gyan9427@gmail.com>
2021-08-31 12:00:33 +0530

    implement ImplementAlpha class to index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,11 @@
 include "../vendor/autoload.php";
 
 use Src\clients\Client;
+use Src\classes\ImplementAlpha;
 
 $c = new Client();
+
+$ia = new ImplementAlpha();
+
+var_dump($ia->calculate(23,3));
 
